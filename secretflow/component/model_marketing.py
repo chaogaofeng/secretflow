c1220cc180c81dde1323a854a6b8c6e08c840194
chaogaofeng <23396178+chaogaofeng@users.noreply.github.com>
2024-12-20 11:00:40 +0800

    add market

--- secretflow/component/model_marketing.py
+++ secretflow/component/model_marketing.py
@@ -136,12 +136,12 @@ def ss_compare_eval_fn(
         rule_party: os.path.join(ctx.data_dir, rule_path_info[rule_party].uri),
     }
     logging.info(f"{data_party}, {ctx.data_dir}, {data_path_info[data_party].uri}")
-    # uri = {
-    #     data_party: data_path_info[data_party].uri,
-    #     rule_party: rule_path_info[rule_party].uri,
-    # }
-    # with ctx.tracer.trace_io():
-    #     download_files(ctx, uri, input_path)
+    uri = {
+        data_party: data_path_info[data_party].uri,
+        rule_party: rule_path_info[rule_party].uri,
+    }
+    with ctx.tracer.trace_io():
+        download_files(ctx, uri, input_path, overwrite=False)
 
     # get spu config from ctx
     if ctx.spu_configs is None or len(ctx.spu_configs) == 0:
